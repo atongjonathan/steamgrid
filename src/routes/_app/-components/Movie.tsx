d02e61f157eeccbc7cb00126a3d0f02034e9e508
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2025-06-03 17:08:39 +0300

    feat: movie title

--- src/routes/_app/-components/Movie.tsx
+++ src/routes/_app/-components/Movie.tsx
@@ -9,7 +9,7 @@ const Movie = ({ movie }: { movie: MinMovie }) => {
     const [loaded, setLoaded] = useState(false);
 
     return (
-        <div className="p-1 hover:scale-95 transition-transform relative rounded overflow-hidden">
+        <div className="p-1 hover:scale-95 transition-transform relative rounded overflow-hidden flex flex-col gap-2">
             <Link to={`/watch/$id`} params={{ id: movie.id.toString() }} className="block w-full aspect-[216/319] relative">
                 {!loaded && (
                     <Skeleton
@@ -28,6 +28,12 @@ const Movie = ({ movie }: { movie: MinMovie }) => {
                     className={`w-full h-full object-cover absolute top-0 left-0 rounded-lg transition-opacity duration-300 ${loaded ? 'opacity-100' : 'opacity-0'}`}
                 />
             </Link>
+            <a className="flex w-full flex-col gap-1" href="/watch/movie/552524">
+                
+                <div className="flex w-full text-[.82rem] sm:text-sm font-medium !line-clamp-2 tracking-wider">
+                    {movie.title}
+                </div>
+            </a>
         </div>
     );
 };
