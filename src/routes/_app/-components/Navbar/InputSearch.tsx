eb59de747637773e7337cd8006a5b34db34ae559
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2025-06-07 23:49:26 +0300

    feat: search

--- src/routes/_app/-components/Navbar/InputSearch.tsx
+++ src/routes/_app/-components/Navbar/InputSearch.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Card, CardContent } from '@/components/ui/card'
 import { cn } from '@/lib/utils'
@@ -7,56 +7,48 @@ import { useQuery } from '@tanstack/react-query'
 import { getMovies } from '@/api'
 import { Search, Star } from 'lucide-react'
 import { isMobile } from 'react-device-detect';
-import { useLocation, useNavigate } from '@tanstack/react-router'
+import { useLocation, useNavigate, useSearch } from '@tanstack/react-router'
 import Skeleton from 'react-loading-skeleton'
-
-const mockResults = [
-    {
-        id: 1,
-        title: 'React Crash Course',
-        thumbnail: 'https://img.youtube.com/vi/w7ejDZ8SWv8/mqdefault.jpg',
-    },
-    {
-        id: 2,
-        title: 'ShadCN UI Tutorial',
-        thumbnail: 'https://img.youtube.com/vi/4nC4VXHdyXY/mqdefault.jpg',
-    },
-    {
-        id: 3,
-        title: 'Understanding Tailwind CSS',
-        thumbnail: 'https://img.youtube.com/vi/dFgzHOX84xQ/mqdefault.jpg',
-    },
-    {
-        id: 4,
-        title: 'YouTube Clone with React',
-        thumbnail: 'https://img.youtube.com/vi/NT299zIk2JY/mqdefault.jpg',
-    },
-    {
-        id: 5,
-        title: 'Advanced TypeScript Tips',
-        thumbnail: 'https://img.youtube.com/vi/d56mG7DezGs/mqdefault.jpg',
-    },
-]
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 export default function InputSearch() {
-    const [query, setQuery] = useState('')
     const [isFocused, setIsFocused] = useState(false)
     const navigate = useNavigate()
     const { pathname } = useLocation()
+    const search = useSearch({ strict: false }) // or define a search schema in route
+    const queryFromURL = (search as Record<string, string>).q || ""
+    const [query, setQuery] = useState(queryFromURL)
 
 
-    const filteredResults = query
-        ? mockResults.filter((r) =>
-            r.title.toLowerCase().includes(query.toLowerCase())
-        )
-        : mockResults
+    useEffect(() => {
+        if (queryFromURL !== query) {
+            setQuery(queryFromURL)
+        }
+    }, [queryFromURL])
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (query !== queryFromURL) {
+                navigate({
+                    search: (prev) => ({
+                        ...prev,
+                        q: query || undefined, // undefined removes the param
+                    }) as never,
+                    replace: true, // avoid pushing history every keystroke
+                })
+            }
+        }, 200)
+
+        return () => clearTimeout(timeout)
+    }, [query])
+
 
     return (
         <div className="relative lg:w-[400px] w-full">
             <div className="w-full flex items-center">
                 <Input
                     startIcon={Search}
-                    placeholder="Search"
+                    placeholder="Search movie or concept"
                     value={query}
                     onChange={(e) => {
                         console.log(e.target.value);
@@ -75,7 +67,7 @@ export default function InputSearch() {
                 />
             </div>
 
-            {isFocused && filteredResults.length > 0 && (
+            {isFocused && (
                 <Card className="absolute left-0 right-0 z-10 mt-2 rounded-xl shadow-md bg-background border text-white">
                     <SearchCardContents />
                 </Card>
@@ -86,41 +78,86 @@ export default function InputSearch() {
 }
 
 export function SearchCardContents() {
+    const search = (useSearch({ strict: false }) as Record<string, string>).q || ""
     const { isLoading, data } = useQuery({
-        queryKey: ["sliderQuery",],
+        queryKey: ["search", search],
         queryFn: () => {
-            return getMovies({ params: { limit: 7 } })
+            return getMovies({ params: { limit: 7, search } })
         },
         staleTime: Infinity,
     })
+
     return (
         <CardContent className="p-0">
             {
-                isLoading ? <Skeleton baseColor='#080a1a' className='px-4 py-3 lg:animate-pulse' /> :
-                    data?.results.map((result) => (
-                        <div
-                            key={result.id}
-                            className={cn(
-                                'flex items-center gap-4 px-4 py-3 cursor-pointer hover:bg-white/10 transition-colors'
-                            )}
-                        >
-                            <img
-                                src={result.poster}
-                                alt={result.title}
-                                className="w-16 h-10 object-cover rounded-md"
-                            />
-                            <div className="text-sm font-medium text-foreground">
-                                {result.title} ({result.year})
-                                <div className='flex gap-2 items-center'>
-                                    <Star color='rgb(255 176 0)' fill='rgb(255 176 0)' size={12} />
-                                    {
-                                        result.rating_star
-                                    }
-
-                                </div>
+                isLoading ? (
+                    <div
+                        className={cn(
+                            'flex items-center gap-4 px-4 py-3 cursor-pointer hover:bg-white/10 transition-colors'
+                        )}
+                    >
+                        <Skeleton
+                            className="w-16 h-10 object-cover rounded-md"
+                        />
+                        <div className="text-sm font-medium text-foreground">
+                            <p><Skeleton /></p>
+                            <div className='flex gap-2 items-center'>
+                                <Skeleton />
                             </div>
                         </div>
-                    ))}
+                    </div>
+                ) : (
+                    <>
+                        {
+
+                            data?.count ?? 0 > 0 ? <>
+                                {
+                                    data?.results.map((result) => (
+                                        <div
+                                            key={result.id}
+                                            className={cn(
+                                                'flex items-center gap-4 px-4 py-3 cursor-pointer hover:bg-white/10 transition-colors'
+                                            )}
+                                        >
+                                            <LazyLoadImage
+                                                src={result.poster}
+                                                alt={result.title}
+                                                className="w-16 h-10 object-cover rounded-md"
+                                                placeholder={(
+                                                    <div><Skeleton baseColor='rgb(22 28 63)' width={60} height={30}/></div>
+                                                )}
+                                            />
+                                            <div className="text-sm font-medium text-foreground">
+                                                {result.title} ({result.year})
+                                                <div className='flex gap-2 items-center'>
+                                                    <Star color='rgb(255 176 0)' fill='rgb(255 176 0)' size={12} />
+                                                    {
+                                                        result.rating_star
+                                                    }
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                    )
+
+                                }
+                            </> : (
+                                <div
+                                    className={cn(
+                                        'flex justify-between gap-4 px-4 py-3 transition-colors text-sm font-medium text-foreground'
+                                    )}
+                                >
+                                    <p>No movie found</p>
+                                    <a href="https://t.me/dont_be_soy2" rel='noopener' target='_blank' className='underline'>
+                                        Request</a>
+                                </div>
+                            )
+                        }
+                    </>
+                )
+
+            }
         </CardContent>
     )
 }
