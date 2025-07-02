593370ca12a7764d2f2f43d17675ebac19186e15
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2025-07-02 10:04:38 +0000

    feat: dialog content

--- src/components/modals/Login.tsx
+++ src/components/modals/Login.tsx
@@ -1,157 +1,155 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import z from "zod"
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import z from "zod";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { toast } from "sonner"
-import { useMutation } from "@tanstack/react-query"
-import { getErrorMessage, login } from "@/api"
-import type { AxiosError } from "axios"
-import { Loader2Icon } from "lucide-react"
-import { useAuth } from "../context/AuthContext"
-import Cookies from "js-cookie"
-
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import { useMutation } from "@tanstack/react-query";
+import { getErrorMessage, login } from "@/api";
+import type { AxiosError } from "axios";
+import { Loader2Icon } from "lucide-react";
+import { useAuth } from "../context/AuthContext";
+import Cookies from "js-cookie";
 
 type LoginProps = {
-    loginClass: string, loginProps: {
-        isLoginOpen: boolean;
-        setisLoginOpen: React.Dispatch<React.SetStateAction<boolean>>;
-        setIsSignUpOpen: React.Dispatch<React.SetStateAction<boolean>>;
-
-    }
-}
+  loginClass: string;
+  loginProps: {
+    isLoginOpen: boolean;
+    setisLoginOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    setIsSignUpOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  };
+};
 export default function Login({ loginClass, loginProps }: LoginProps) {
-    const { isLoginOpen, setisLoginOpen, setIsSignUpOpen } = loginProps
-    const { fetchUser } = useAuth()
+  const { isLoginOpen, setisLoginOpen, setIsSignUpOpen } = loginProps;
+  const { fetchUser } = useAuth();
 
-    const FormSchema = z.object({
-        username: z.string().min(2, {
-            message: "Username must be at least 2 characters.",
-        }),
-        password: z.string().min(4, {
-            message: "Password must be at least 4 characters.",
-        }),
-    });
+  const FormSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    password: z.string().min(4, {
+      message: "Password must be at least 4 characters.",
+    }),
+  });
 
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-            username: "",
-            password: "",
-        },
-    });
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
 
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["login"],
+    mutationFn: login,
+    onSuccess: async ({ access, refresh }) => {
+      Cookies.set("access", access);
+      Cookies.set("refresh", refresh);
+      setisLoginOpen(false);
+      toast.success("Logged in");
+      await fetchUser();
+    },
+    onError: (err: AxiosError) => {
+      toast.error(getErrorMessage(err));
+    },
+  });
 
-    const { mutate, isPending } = useMutation(
-        {
-            mutationKey: ["login"],
-            mutationFn: login,
-            onSuccess: async ({ access, refresh }) => {
-                Cookies.set("access", access)
-                Cookies.set("refresh", refresh)
-                setisLoginOpen(false)
-                toast.success("Logged in")
-                await fetchUser()
-            },
-            onError: (err: AxiosError) => {
-                toast.error(getErrorMessage(err));
-            }
-        }
-    )
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    mutate(data);
+  }
 
-    function onSubmit(data: z.infer<typeof FormSchema>) {
-        mutate(data)
-    }
+  function openSignUp() {
+    setisLoginOpen(false);
+    setIsSignUpOpen(true);
+  }
 
-    function openSignUp() {
-        setisLoginOpen(false)
-        setIsSignUpOpen(true)
-    }
+  return (
+    <Dialog open={isLoginOpen} onOpenChange={setisLoginOpen}>
+      <DialogTrigger asChild>
+        <Button className={loginClass} variant="outline">
+          Login
+        </Button>
+      </DialogTrigger>
 
+      <DialogContent className="sm:max-w-[425px]" aria-describedby="Login">
+        <DialogHeader>
+          <DialogTitle>Login</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>Login</DialogDescription>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-    return (
-        <Dialog open={isLoginOpen} onOpenChange={setisLoginOpen}>
-            <DialogTrigger asChild>
-                <Button className={loginClass} variant="outline">
-                    Login
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="••••••••" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="text-sm">
+              Don&apos;t have an account?{" "}
+              <button
+                type="button"
+                className="underline underline-offset-4 text-subMain"
+                onClick={openSignUp}
+              >
+                Sign up
+              </button>
+            </div>
+
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="button" variant="outline">
+                  Cancel
                 </Button>
-            </DialogTrigger>
-
-            <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                    <DialogTitle>Login</DialogTitle>
-                </DialogHeader>
-
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-                        <FormField
-                            control={form.control}
-                            name="username"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Username</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="username" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-
-                        <FormField
-                            control={form.control}
-                            name="password"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <FormControl>
-                                        <Input type="password" placeholder="••••••••" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-
-                        <div className="text-sm">
-                            Don&apos;t have an account?{" "}
-                            <button type="button" className="underline underline-offset-4 text-subMain" onClick={openSignUp}>
-                                Sign up
-                            </button>
-                        </div>
-
-                        <DialogFooter>
-                            <DialogClose asChild>
-                                <Button type="button" variant="outline">
-                                    Cancel
-                                </Button>
-                            </DialogClose>
-                            <Button type="submit">
-                                {
-                                    isPending && <Loader2Icon className="animate-spin" />
-                                }
-                                Login</Button>
-
-                        </DialogFooter>
-                    </form>
-                </Form>
-            </DialogContent>
-        </Dialog>
-    );
+              </DialogClose>
+              <Button type="submit">
+                {isPending && <Loader2Icon className="animate-spin" />}
+                Login
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
 }
--- src/components/modals/SignUp.tsx
+++ src/components/modals/SignUp.tsx
@@ -1,15 +1,16 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import z from "zod"
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import z from "zod";
 import {
   Form,
   FormControl,
@@ -17,26 +18,25 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { toast } from "sonner"
-import { useMutation } from "@tanstack/react-query"
-import { signUp } from "@/api"
-import type { AxiosError } from "axios"
-import { Loader2Icon } from "lucide-react"
-
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import { useMutation } from "@tanstack/react-query";
+import { signUp } from "@/api";
+import type { AxiosError } from "axios";
+import { Loader2Icon } from "lucide-react";
 
 type SignUpProps = {
-  loginClass: string, signUpProps: {
+  loginClass: string;
+  signUpProps: {
     isSignUpOpen: boolean;
     setIsSignUpOpen: React.Dispatch<React.SetStateAction<boolean>>;
     setisLoginOpen: React.Dispatch<React.SetStateAction<boolean>>;
-
-  }
-}
+  };
+};
 export default function SignUp({ loginClass, signUpProps }: SignUpProps) {
-  const { isSignUpOpen, setIsSignUpOpen, setisLoginOpen } = signUpProps
+  const { isSignUpOpen, setIsSignUpOpen, setisLoginOpen } = signUpProps;
 
   const FormSchema = z.object({
     username: z.string().min(2, {
@@ -57,60 +57,66 @@ export default function SignUp({ loginClass, signUpProps }: SignUpProps) {
     },
   });
 
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["signUp"],
+    mutationFn: signUp,
+    onSuccess: () => {
+      toast.success("Sign up successful");
+      openLogin();
+    },
+    onError: (err: AxiosError) => {
+      const errorData = err.response?.data as Record<string, any> | undefined;
 
-  const { mutate, isPending } = useMutation(
-    {
-      mutationKey: ["signUp"],
-      mutationFn: signUp,
-      onSuccess: () => {
-        toast.success("Sign up successful")
-        openLogin()
-      },
-      onError: (err: AxiosError) => {
-        const errorData = err.response?.data as Record<string, any> | undefined;
+      if (errorData) {
+        const emailError = errorData.email;
+        const usernameError = errorData.username;
+        const passwordError = errorData.password;
 
-        if (errorData) {
-          const emailError = errorData.email;
-          const usernameError = errorData.username;
-          const passwordError = errorData.password;
-
-          if (emailError) {
-            form.setError("email", {
+        if (emailError) {
+          form.setError(
+            "email",
+            {
               message: emailError,
-            }, {
+            },
+            {
               shouldFocus: true,
-            });
-          }
-
-          if (usernameError) {
-            form.setError("username", {
-              message: usernameError,
-            }, {
-              shouldFocus: !emailError, // Only focus if email didn't already
-            });
-          }
-
-          if (passwordError) {
-            form.setError("password", {
-              message: passwordError,
-            }, {
-              shouldFocus: !emailError && !usernameError, // Focus only if others didn't
-            });
-          }
+            }
+          );
         }
 
+        if (usernameError) {
+          form.setError(
+            "username",
+            {
+              message: usernameError,
+            },
+            {
+              shouldFocus: !emailError, // Only focus if email didn't already
+            }
+          );
+        }
 
-
+        if (passwordError) {
+          form.setError(
+            "password",
+            {
+              message: passwordError,
+            },
+            {
+              shouldFocus: !emailError && !usernameError, // Focus only if others didn't
+            }
+          );
+        }
       }
-    }
-  )
+    },
+  });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
-    mutate(data)
+    mutate(data);
   }
   function openLogin() {
-    setIsSignUpOpen(false)
-    setisLoginOpen(true)
+    setIsSignUpOpen(false);
+    setisLoginOpen(true);
   }
 
   return (
@@ -125,7 +131,7 @@ export default function SignUp({ loginClass, signUpProps }: SignUpProps) {
         <DialogHeader>
           <DialogTitle>Sign up</DialogTitle>
         </DialogHeader>
-
+        <DialogDescription>Sign up</DialogDescription>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
             <FormField
@@ -149,7 +155,11 @@ export default function SignUp({ loginClass, signUpProps }: SignUpProps) {
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input type="email" placeholder="you@example.com" {...field} />
+                    <Input
+                      type="email"
+                      placeholder="you@example.com"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -171,8 +181,12 @@ export default function SignUp({ loginClass, signUpProps }: SignUpProps) {
             />
 
             <div className="text-sm">
-              Already have an account? {" "}
-              <button type="button" className="underline underline-offset-4 text-subMain" onClick={openLogin}>
+              Already have an account?{" "}
+              <button
+                type="button"
+                className="underline underline-offset-4 text-subMain"
+                onClick={openLogin}
+              >
                 Login
               </button>
             </div>
@@ -183,10 +197,9 @@ export default function SignUp({ loginClass, signUpProps }: SignUpProps) {
                 </Button>
               </DialogClose>
               <Button type="submit">
-                {
-                  isPending && <Loader2Icon className="animate-spin" />
-                }
-                Sign Up</Button>
+                {isPending && <Loader2Icon className="animate-spin" />}
+                Sign Up
+              </Button>
             </DialogFooter>
           </form>
         </Form>
